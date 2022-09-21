eab5025659681593af8cd012f7c0b55475128222
vijaylokanadam <103408223+vijaylokanadam@users.noreply.github.com>
2022-09-21 12:32:17 +0530

    second version

--- App.java
+++ App.java
@@ -6,7 +6,7 @@ public class App {
 		// TODO Auto-generated method stub
 
 		IJob j1=new Opportunity();
-		String[]jobs = j1.findJobsByCity("Bangalore");
+		String[]jobs = j1.findJobsBysalary(10000);
 		for(String j : jobs)
 			System.out.println(j);
 		
